edb4dff24b1be25699e48f2bd43d832df8706570
niklas-hjelm <niklas.hjelm@iths.se>
2022-10-05 10:36:15 +0200

    Add project files.

---
+++ DemoWPF/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DemoWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(e.Source is Button btn)
+            {
+                if(btn.Content != null)
+                    MinText.Text = btn.Content.ToString();
+            }
+            else
+            {
+
+                MinText.Text = Input.Text;
+            }
+        }
+    }
+}
